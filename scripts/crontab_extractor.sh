d93eb9b59775b84a6913eb13aa8d6d4d75cbf42f
Jason Christiansen <jason.christiansen@hellofresh.com>
2024-09-25 08:53:35 -0500

    make directories if needed

--- scripts/crontab_extractor.sh
+++ scripts/crontab_extractor.sh
@@ -9,6 +9,9 @@ echo "script_dir: $script_dir"
 directory_path=$(realpath "$script_dir/../triggers")
 echo "directory_path: $directory_path"
 
+# Create the directory if it doesn't exist
+mkdir -p "$directory_path"
+
 FILENAME="crontab_extraction_${HOSTNAME}.txt"
 
 # Define save location
@@ -16,6 +19,6 @@ save_path="${directory_path}/${FILENAME}"
 
 echo "$(date +"%Y-%m-%d %H:%M:%S") Starting crontab extraction, storing in $save_path"
 
-crontab -l > "$save_path"
+crontab -l >"$save_path"
 
 echo "$(date +"%Y-%m-%d %H:%M:%S") Completed crontab extraction"
