e94439da9883babd7d4efc31907e6880f17e429b
Jason Christiansen <jason.christiansen@hellofresh.com>
2025-02-26 10:27:52 -0600

    update script config

--- scripts/config_scripts.py
+++ scripts/config_scripts.py
@@ -13,6 +13,7 @@ grandparent_dir = os.path.dirname(parent_dir)
 great_grandparent_dir = os.path.dirname(grandparent_dir)
 
 data_dir = os.path.join(parent_dir, "data")
+schema_dir = os.path.join(parent_dir, "schemas")
 log_dir = os.path.join(parent_dir, "logs")
 docs_dir = os.path.join(parent_dir, "docs")
 src_dir = os.path.join(parent_dir, "src")
