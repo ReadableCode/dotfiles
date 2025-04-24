2842a3ff58d935d408953c84f3ddef29b8c8033a
jason <jasonwchristiansen@gmail.com>
2025-04-23 20:41:24 -0500

    sync config scripts

--- src/config.py
+++ src/config.py
@@ -3,26 +3,30 @@
 
 import os
 import sys
+from os.path import expanduser
 
+home_dir = expanduser("~")
 file_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 grandparent_dir = os.path.dirname(parent_dir)
 great_grandparent_dir = os.path.dirname(grandparent_dir)
 
 data_dir = os.path.join(parent_dir, "data")
+templates_dir = os.path.join(parent_dir, "templates")
 log_dir = os.path.join(parent_dir, "logs")
 src_dir = os.path.join(parent_dir, "src")
 src_utils_dir = os.path.join(src_dir, "utils")
 drive_download_cache_dir = os.path.join(data_dir, "drive_download_cache")
-email_attachment_dir = os.path.join(data_dir, "email_attachments")
+s3_download_cache = os.path.join(data_dir, "s3_download_cache")
 
 directories = [
     data_dir,
+    templates_dir,
     log_dir,
     src_dir,
     src_utils_dir,
     drive_download_cache_dir,
-    email_attachment_dir,
+    s3_download_cache,
 ]
 for directory in directories:
     if not os.path.exists(directory):
@@ -36,6 +40,7 @@ sys.path.append(src_dir)
 sys.path.append(src_utils_dir)
 
 if __name__ == "__main__":
+    print(f"home_dir: {home_dir}")
     print(f"file_dir: {file_dir}")
     print(f"parent_dir: {parent_dir}")
     print(f"grandparent_dir: {grandparent_dir}")
