6179ec5150610d59e9b836e893bb1da8dd39f502
jason <jasonwchristiansen@gmail.com>
2025-04-23 21:58:14 -0500

    update deployments

--- scripts/config_scripts.py
+++ scripts/config_scripts.py
@@ -6,18 +6,32 @@ import sys
 from os.path import expanduser
 
 home_dir = expanduser("~")
-
 file_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 grandparent_dir = os.path.dirname(parent_dir)
 great_grandparent_dir = os.path.dirname(grandparent_dir)
 
 data_dir = os.path.join(parent_dir, "data")
-schema_dir = os.path.join(parent_dir, "schemas")
+templates_dir = os.path.join(parent_dir, "templates")
 log_dir = os.path.join(parent_dir, "logs")
-docs_dir = os.path.join(parent_dir, "docs")
 src_dir = os.path.join(parent_dir, "src")
 src_utils_dir = os.path.join(src_dir, "utils")
+drive_download_cache_dir = os.path.join(data_dir, "drive_download_cache")
+s3_download_cache = os.path.join(data_dir, "s3_download_cache")
+
+directories = [
+    data_dir,
+    templates_dir,
+    log_dir,
+    src_dir,
+    src_utils_dir,
+    drive_download_cache_dir,
+    s3_download_cache,
+]
+for directory in directories:
+    if not os.path.exists(directory):
+        print(f"Creating directory: {directory}")
+        os.makedirs(directory)
 
 sys.path.append(file_dir)
 sys.path.append(parent_dir)
@@ -31,6 +45,5 @@ if __name__ == "__main__":
     print(f"parent_dir: {parent_dir}")
     print(f"grandparent_dir: {grandparent_dir}")
     print(f"data_dir: {data_dir}")
-    print(f"src_dir: {src_dir}")
 
 # %%
