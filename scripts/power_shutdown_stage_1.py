eb3ed0d24f989c73724287dec283d1d8ba390b57
jason <jasonwchristiansen@gmail.com>
2025-09-22 17:01:01 -0500

    add shutdown script

---
+++ scripts/power_shutdown_stage_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import datetime
+import sys
+
+# NUT calls your script with the command name as the first argument
+event = sys.argv[1] if len(sys.argv) > 1 else "UNKNOWN"
+
+with open("power_shutdown_stage_1.log", "a") as f:
+    f.write(f"[{datetime.datetime.now()}] Event: {event}\n")
+    f.flush()
