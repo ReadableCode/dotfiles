79b906e7f2ab758dfab8079b3f7012f8ed9d50ac
jason <jason.christiansen@fourteenfoods.com>
2025-11-05 05:16:33 -0600

    add fzf path

--- application_configs/powershell/powershell_aliases.ps1
+++ application_configs/powershell/powershell_aliases.ps1
@@ -5,7 +5,8 @@ if ($env:COMPUTERNAME -eq 'FFLAP-2229') {
         'C:\Users\jason.christiansen\userapps\nvim-win64\bin',
         'C:\Users\jason.christiansen\userapps\WPy64-31350\python',
         'C:\Users\jason.christiansen\userapps\WPy64-31350\python\Scripts',
-        'C:\Users\jason.christiansen\userapps\PortableGit\bin'
+        'C:\Users\jason.christiansen\userapps\PortableGit\bin',
+        'C:\Users\jason.christiansen\userapps\fzf-0.66.1-windows_amd64'
     )
     $curr = ($env:Path -split ';') | Where-Object { $_ }
     foreach ($p in $paths) {
