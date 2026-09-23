72cc028c7870333fc7d9f795f4318adb7761822b
jason <jasonwchristiansen@gmail.com>
2026-09-23 15:48:42 -0500

    remove the aws credential extractor copies, the na-finops scripts are the only ones

--- scripts/aws_credential_extractor_kube.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Kube/EKS credentials. Wrapper around aws_credential_extractor.sh.
-# Usage: source aws_credential_extractor_kube.sh
-source "$(dirname "${BASH_SOURCE[0]}")/aws_credential_extractor.sh" --profile sso-eks-dwh-basic --role sso_eks_basic --region eu-west-1
